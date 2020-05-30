f347ebf2fe25f992eb3f96198f253a183b7aaddb
BigDog1400 <63280663+BigDog1400@users.noreply.github.com>
2020-05-30 17:44:17 -0400

    Primera version estable

---
+++ js/content.js
@@ -0,0 +1,37 @@
+const textToCopy = [];
+
+const copyToClipboard = textToCopy => {
+  console.log(textToCopy);
+  const el = document.createElement("textarea");
+  el.value = `${
+    textToCopy.length <= 1
+      ? textToCopy[0]
+      : textToCopy
+          .map((text, index) => {
+            if (index !== textToCopy.length - 1) {
+              return `${text}, `;
+            } else {
+              return `and ${text}.`;
+            }
+          })
+          .join("")
+  }`;
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+};
+
+document.addEventListener("click", MouseEvent => {
+  if (MouseEvent.altKey) {
+    MouseEvent.preventDefault();
+    text = MouseEvent.target.innerText;
+    if (text !== undefined && text.length > 0) {
+      textToCopy.push(text);
+      copyToClipboard(textToCopy);
+    }
+  }
+});
